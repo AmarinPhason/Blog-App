3ca0a55e20e4c5e69adb9375bd32ed8a27d63d2f
amarin phason <thedarksidest@gmail.com>
2024-07-28 12:02:07 +0700

    create pages and routes

--- clientGPT/src/App.jsx
+++ clientGPT/src/App.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 export default function App() {
-  return <div className="">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
